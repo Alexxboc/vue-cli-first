29838ad9b884749b13f5f66c31ebe23012523fe0
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-02 16:40:36 +0200

    complete milestone 1

---
+++ src/components/title.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="title">
+        <h1><em>"La mia prima app con Vue CLI!"</em></h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Title"
+}
+</script>
+
+<style scoped>
+    .title {
+        color: aquamarine;
+    }
+</style>
